a4840f13fbbe566eb368b6d69039f789d676877f
Dhruv Garg <dhruvgarg086@gmail.com>
2025-04-20 10:47:54 +0530

    List Code -> 19. Remove Nth Node From End of List

--- Day5_Linked List/removeNthNodeFromEnd.cpp
+++ Day5_Linked List/removeNthNodeFromEnd.cpp
@@ -1,9 +1,8 @@
 // Leet Code -> 19. Remove Nth Node From End of List
 
-// Given the head of a linked list, 
-// remove the nth node from the end of 
+// Given the head of a linked list,
+// remove the nth node from the end of
 // the list and return its head.
-
 
 // TC -> O(N)
 // SC -> O(1)
@@ -18,28 +17,25 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-    public:
-        ListNode* removeNthFromEnd(ListNode* head, int n) {
-            ListNode* fastp = head;
-            ListNode* slowp = head;
-            for(int i = 0; i < n; i++)
-            {
-                fastp = fastp -> next;
-            }
-            if(fastp == NULL)
-            {
-                return head;
-            }
-            while(fastp != NULL)
-            {
-                fastp = fastp -> next;
-                slowp = slowp -> next;
-            }
-            ListNode* delnode = slowp -> next;
-            slowp->next = slowp->next->next;
-            delete delnode;
-            return head;
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode *start = new ListNode();
+        start->next = head;
+        ListNode *fast = start;
+        ListNode *slow = start;
+        for (int i = 0; i < n; i++)
+        {
+            fast = fast->next;
         }
-    
-    };
+        while (fast->next != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        return start->next;
+    }
+};
